ece51e3ef3476cf87ed54476ffb406174f6277fe
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-03-16 17:06:47 +0100

    [STARTING PROJECT]

---
+++ src/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+##
+## EPITECH PROJECT, 2020
+## Alexandre Wagner - Victor Rouxel
+## File description:
+## Groundhog projet
+##
+
+import sys
+
+def groundhog_end():
+
+
+def groundhog():
+    p = sys.argv[1]
+
+    while(1):
+        user_input = input()
+        if user_input == 'STOP'
+            groundhog_end()
+        else
+
+
+def help():
+    print("SYNOPSIS")
+    print("\t./groundhog period")
+    print("")
+    print("DESCRIPTION")
+    print("\tperiod\tthe number of days defining a period")
+
+def parsing():
+    if len(sys.argv) < 2:
+        help()
+        exit()
+    if sys.argv == "-h"
+        help()
+        exit()
+
+
+if __name__ == '__main__':
+    parsing()
+    groundhog()
