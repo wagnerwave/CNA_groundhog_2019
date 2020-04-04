78502454e3ec5e2723458730ea20bb7f8d8907c0
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-04-04 18:32:39 +0200

    Recalculation des weirdest values [ Mode DEBUG : ACTIVER ]

--- src/groundhog.py
+++ src/groundhog.py
@@ -7,9 +7,10 @@
 ## Groundhog projet
 ##
 
+from statistics import *
+import statistics as stats
 import sys
 import math
-import statistics as stats
 
 class Groundhog:
     def __init__(self):
@@ -61,39 +62,10 @@ class Groundhog:
                 self.calcul_weather(user_input)
                 self.display()
 
-    def AdditionTemperatureByPeriod(self):
-        i = 0
-        result = 0
-
-        while (i < self._period):
-            result = result + self._temperature[i - self._period]
-            i = i + 1
-        return (result)
-
-    def AddWeirdestValue(self, user_input):
-        mobileAverage = (1 / self._period) * self.AdditionTemperatureByPeriod()
-        HighBand = mobileAverage + 2 * float(self._s)
-        LowBand = mobileAverage - 2 * float(self._s)
-        List = []
-        diff = 0
-
-        List.append(user_input)
-        try:
-            diff = (float(user_input) - LowBand) / (HighBand / LowBand)
-        except ZeroDivisionError:
-            diff = 0
-        if (diff > 0.5):
-            List.append(1 - diff)
-        else:
-            List.append(diff)
-        self._weirdestValueList.append(List)
-
     def calcul_weather(self, user_input):
         self.temperatureIncreaseAverage() # calcul for g value (self._g)
         self.relativeTemperatureEvolution() # calcul for r value (self._r)
         self.standardDeviation() # calcul for s value (self._s)
-        if (len(self._temperature) >= self._period):
-            self.AddWeirdestValue(user_input)
 
     def temperatureIncreaseAverage(self):
         if (len(self._temperature) >= self._period):
@@ -140,6 +112,37 @@ class Groundhog:
         else:
             return False
 
+    def getTheFiveWeirdestValue(self):
+        self._weirdestValueList = self._temperature
+        self._weirdestValueList.sort()
+        if (len(self._weirdestValueList) % 2) == 0:
+            M = median(self._weirdestValueList)
+        else:
+            M = ((median_low(self._weirdestValueList) + median_high(self._weirdestValueList)) / 2)
+        if (len(self._weirdestValueList) % 4) == 0:
+            Q1 = self._weirdestValueList[len(self._weirdestValueList)//4 - 1]
+            Q3 = self._weirdestValueList[3 * len(self._weirdestValueList)//4 - 1]
+        else:
+            Q1 = self._weirdestValueList[len(self._weirdestValueList)//4]
+            Q3 = self._weirdestValueList[3 * len(self._weirdestValueList)//4]
+        print("Median :", M)
+        print("Q1: ", Q1)
+        print("Q3: ", Q3)
+        InterQ = Q3 - Q1
+        print("InterQ : ", InterQ)
+        InterLimit = InterQ * 1.5
+        print("Inter Limit :", InterLimit)
+        InterLimitInf = Q1 - InterLimit
+        InterLimitSup = Q3 + InterLimit
+        print("InterLimitInf : ", InterLimitInf)
+        print("InterLimitSup : ", InterLimitSup)
+        array = []
+        print("value user:", self._weirdestValueList)
+        for i in self._weirdestValueList:
+            if (self._weirdestValueList[int(i)] < InterLimitInf or self._weirdestValueList[int(i)] > InterLimitSup):
+                array.append(self._weirdestValueList[int(i)])
+        print("Array : ", array)
+
     def display(self):
         if (self._r != "nan" and self._g != "nan" and self._s != "nan"):
             Weather_message = "g=" + str(round(self._g, 2)) + "\tr=" + str(round(self._r, 2)) + "%\ts=" + str(round(self._s, 2))
@@ -154,16 +157,15 @@ class Groundhog:
             print("g=nan\tr=nan%\ts=nan")
 
     def groundhog_end(self):
-        FiveShapeOfWeirdestValue = []
+
         Message_tendency_witched = "Global tendency switched " + str(self._nbTendency) + " times"
         Message_weirdest_value = str(len(self._weirdestValueList)) + " weirdest values are [" + str(self._weirdestValueList)[1:-1], "]"
 
         print(Message_tendency_witched)
-        self._weirdestValueList.sort(key=lambda x: x[1], reverse = False)
+        self.getTheFiveWeirdestValue()
         if (len(self._weirdestValueList) >= 5):
-            for x in range(0,5):
-                FiveShapeOfWeirdestValue.append(self._weirdestValueList[x][0])
-            print("5 weirdest values are [", ', '.join(FiveShapeOfWeirdestValue), "]")
+            #FiveShapeOfWeirdestValue = self.getTheFiveWeirdestValue(self._weirdestValueList)
+            #print("5 weirdest values are [", ', '.join(FiveShapeOfWeirdestValue), "]"
             exit(0)
         else:
             exit(84)
@@ -171,3 +173,33 @@ class Groundhog:
     def start(self):
         self.parsing()
         self.prompt()
+
+    # self._weirdestValueList.sort(key=lambda x: x[1], reverse = False)
+    #   for x in range(0,5):
+    #       FiveShapeOfWeirdestValue.append(self._weirdestValueList[x][0])
+    # def AdditionTemperatureByPeriod(self):
+    #    i = 0
+    #    result = 0
+    #
+    #    while (i < self._period):
+    #        result = result + self._temperature[i - self._period]
+    #        i = i + 1
+    #    return (result)
+    #
+    # def AddWeirdestValue(self, user_input):
+    #    mobileAverage = (1 / self._period) * self.AdditionTemperatureByPeriod()
+    #    HighBand = mobileAverage + 2 * float(self._s)
+    #   LowBand = mobileAverage - 2 * float(self._s)
+    #   List = []
+    #   diff = 0
+    #
+    #    List.append(user_input)
+    #   try:
+    #       diff = (float(user_input) - LowBand) / (HighBand / LowBand)
+    #   except ZeroDivisionError:
+    #       diff = 0
+    #   if (diff > 0.5):
+    #      List.append(1 - diff)
+    #   else:
+    #       List.append(diff)
+    #   self._weirdestValueList.append(List)
